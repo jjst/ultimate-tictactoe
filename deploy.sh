e98f57efcbca0e6a8d4c642346e3b0d9e4cbf9ab
Jeremie Jost <jeremiejost@gmail.com>
2016-07-14 12:10:11 -0400

    Automatically deploy new github page on commit

---
+++ deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -o errexit -o nounset
+
+rev=$(git rev-parse --short HEAD)
+
+git init
+git config user.name "Jeremie Jost"
+git config user.email "jeremiejost@gmail.com"
+
+git remote add upstream "https://$GH_TOKEN@github.com/jjst/elmtimate-tictactoe.git"
+git fetch upstream
+git reset upstream/gh-pages
+
+
+git add -A tictactoe.js
+git commit -m "Rebuild pages at ${rev}"
+git push -q upstream HEAD:gh-pages
